52ea03816b382df27cdad3d96cbe5aaaa29fb49d
Kyse7535 <ktevot@gmail.com>
2025-03-11 10:05:21 +0100

    day-1--authentication_with-db: initial commit

---
+++ day-1/authentication_with_db/src/test/java/com/example/authentication_with_db/AuthenticationWithDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authentication_with_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationWithDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
