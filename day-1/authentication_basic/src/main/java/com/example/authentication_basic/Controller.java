e27f7101686390b1cd8e891551890b405d05cd6d
Kyse7535 <ktevot@gmail.com>
2025-03-11 09:21:34 +0100

    day-1-->authentication-basic: add aroute and protect all route

---
+++ day-1/authentication_basic/src/main/java/com/example/authentication_basic/Controller.java
@@ -0,0 +1,18 @@
+package com.example.authentication_basic;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@RestController
+@RequestMapping("/test")
+public class Controller {
+
+    @GetMapping
+    public String getMethodName(@RequestParam String param) {
+        return "test";
+    }
+
+}
