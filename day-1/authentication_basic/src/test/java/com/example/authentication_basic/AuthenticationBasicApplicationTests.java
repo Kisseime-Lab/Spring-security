8020fb4d0d59203365bccb7498d1d06640176858
Kyse7535 <ktevot@gmail.com>
2025-03-11 08:36:39 +0100

    day-1-->authentication_bacic: initial commit

---
+++ day-1/authentication_basic/src/test/java/com/example/authentication_basic/AuthenticationBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authentication_basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
