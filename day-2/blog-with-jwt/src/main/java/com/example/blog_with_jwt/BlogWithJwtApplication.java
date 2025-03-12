150a77ae5da7de1fcf5823999849d6a73ad41df7
Kyse7535 <ktevot@gmail.com>
2025-03-12 13:41:14 +0100

    day-2--blog-with-jwt: initial commit

---
+++ day-2/blog-with-jwt/src/main/java/com/example/blog_with_jwt/BlogWithJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.blog_with_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogWithJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogWithJwtApplication.class, args);
+	}
+
+}
---
+++ day-2/blog-with-jwt/src/test/java/com/example/blog_with_jwt/BlogWithJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blog_with_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogWithJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
