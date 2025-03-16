077b075e53d83b4faadcc22132c9fa265d24ff97
Kyse7535 <ktevot@gmail.com>
2025-03-16 10:23:00 +0100

    day-4--oauth2-client-get-repo-github: initial commit

---
+++ day-4/oauth2-client-get-github-repo/src/main/java/com/example/oauth2_client_get_github_repo/Oauth2ClientGetGithubRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauth2_client_get_github_repo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2ClientGetGithubRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2ClientGetGithubRepoApplication.class, args);
+	}
+
+}
---
+++ day-4/oauth2-client-get-github-repo/src/test/java/com/example/oauth2_client_get_github_repo/Oauth2ClientGetGithubRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauth2_client_get_github_repo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2ClientGetGithubRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
